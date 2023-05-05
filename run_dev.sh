76f9b37b55799289ae6bc695483248db99a3d452
John Chen <johnchen@chromium.org>
2023-05-05 08:38:34 -0700

    Add shell scripts to run bench-o-matic on Mac

---
+++ run_dev.sh
@@ -0,0 +1,1 @@
+python3 bom.py --runs 22 --full_speedometer2_score=False --full_motionmark_score=False --use_enable_field_trial_config=True --sleep_interval=30 --compare_stable_browsers=False > log_`date -I`_chrome_dev_vs_safari_tp.txt
---
+++ run_stable.sh
@@ -0,0 +1,1 @@
+python3 bom.py --runs 22 --full_speedometer2_score=False --full_motionmark_score=False --use_top_seeds=True --sleep_interval=30 --compare_stable_browsers=True > log_`date -I`_chrome_stable_vs_safari.txt
